9db8ec9bc836f7d21732d2d180c87d0114dee79d
Zach DeFazio <zachiscool1031@gmail.com>
2019-01-14 14:41:25 -0500

    Fixed color bug

--- Assets/Scripts/TeleportController.cs
+++ Assets/Scripts/TeleportController.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class TeleportController : MonoBehaviour
 {
+    public GameObject portal;
+    public Material portal_color;
     public GameObject portal_a;
+    public Material portal_a_color;
+    public Material white;
+    public float cooldown = .001f;
+    private float nextTimetoTeleport = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,27 +23,57 @@ public class TeleportController : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
 
-                RaycastHit hit;
-                if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+            {
+                if (hit.collider.tag != "Not-Portal")
                 {
-                        hit.collider.tag = "Portal";
-                    
+                    if (portal != null)
+                    {
+                        portal.GetComponent<Renderer>().material = white;
+                    }
 
+                    portal = hit.collider.gameObject;
+                    hit.collider.GetComponent<Renderer>().material = portal_color;
                 }
 
 
-            
 
+            }
+
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+            {
+                if (hit.collider.tag != "Not-Portal")
+                {
+                    if (portal_a!= null)
+                    {
+                        portal_a.GetComponent<Renderer>().material = white;
+                    }
+                    portal_a = hit.collider.gameObject;
+                    hit.collider.GetComponent<Renderer>().material = portal_a_color;
+                }
+
+            }
         }
     }
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Portal")
+        if (collision.gameObject == portal && Time.time >= nextTimetoTeleport)
         {
 
+            nextTimetoTeleport = Time.time + 1f / cooldown;
             transform.position = portal_a.GetComponent<Transform>().position;
 
 
+        }
+        else if (collision.gameObject == portal_a && Time.time >= nextTimetoTeleport)
+        {
+            nextTimetoTeleport = Time.time + 1f / cooldown;
+            transform.position = portal.GetComponent<Transform>().position;
         }
     }
         
